19ca8d975e4ef206b7b5aa3da3e2ead0dce28790
AmeyaDaddikar <ameyadaddikar@gmail.com>
2018-06-14 10:54:22 +0530

    Changed the structure of the project.
    The project now is divided into the following:
    1. components
    2. pages
    
    Both the components are dumb in nature and are accessed with another php

---
+++ php/pages/aboutus.php
@@ -0,0 +1,23 @@
+<?php
+$GLOBALS['page_title'] = "About Us";
+$GLOBALS['css_includes'] = array("css/aboutus.css");
+?>
+
+<!DOCTYPE html>
+<html>
+	<?php require "headers.php" ?>
+
+	<body>
+		
+		<!--Navigation-->
+		<?php include 'navigation.php'; ?>
+
+	</body>
+
+	<style type="text/css" media="screen">
+    	body{
+     	 	background-color: #000;
+   	 	}  
+ 	</style>
+
+</html>
