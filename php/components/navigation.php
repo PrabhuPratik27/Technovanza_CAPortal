49b1c82cfb7cd4ff68827e062748398779005fb1
AmeyaDaddikar <ameyadaddikar@gmail.com>
2018-06-18 12:06:09 +0530

    1. Added profile nav bar, to be used in other profile pages 2. Added new techno png logo just for testing

--- php/components/navigation.php
+++ php/components/navigation.php
@@ -1,8 +1,7 @@
-<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-    <a class="navbar-brand" href="index"><img src="/Technovanza_CAPortal/images/1.png" width="300px" height="60px")"></a>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+    <a class="navbar-brand" href="index"><img src="/Technovanza_CAPortal/images/techno_logo.png" width="300px" height="60px")"></a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
--- php/pages/profile_template.php
+++ php/pages/profile_template.php
@@ -3,6 +3,12 @@
 	<?php require getcwd()."../../php/components/headers.php"; ?>
 	<body>
 		<?php require getcwd().'../../php/components/navigation.php'; ?>
-		
+
+		<div class="container-fluid row no-spaces">
+			<?php require getcwd().'../../php/components/profile_navbar.php'; ?>
+			<div class="col-md-10">
+				asdasd
+			</div>
+		</div>
 	</body>
 </html>
